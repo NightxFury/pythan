6549375adb1ca97dd884db6a2b688b883d938ea8
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-14 14:19:13 +0530

    Add files via upload

---
+++ practice5.py
@@ -0,0 +1,9 @@
+"""
+Wap to input 2 int numbers a and b .
+print true if a is greater than b and false otherwise
+"""
+a = int(input("enter the value of a :"))
+b = int (input("enter the value of b :"))
+
+print(a > b)
+
