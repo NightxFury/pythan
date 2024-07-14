8b43c2cb874f73a66d1f7cc65f1105bd3cf42597
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-14 14:18:27 +0530

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,7 @@
+""""
+print area of a rectangle
+"""
+width = float(input("type the vale of the width :"))
+height = float(input("type the value of height :"))
+
+print("the area of the rectangle is: ", width * height)
