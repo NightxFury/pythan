21a4050de80298fcd064969eac64004fe0354238
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-14 14:18:54 +0530

    Add files via upload

---
+++ practice4.py
@@ -0,0 +1,7 @@
+"""
+WAP TO INPUT 2 FLOATING NUMBERS AND PRINT THEIR AVARAGE
+"""
+numbers1 = float(input("enter the first number:"))
+numbers2 = float(input("enter the second number :"))
+
+print("avarege is =",( numbers1 +numbers2)/2)
