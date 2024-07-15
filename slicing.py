c8071508f875de2fa3a0fa2a2399f08e94114058
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-16 01:47:18 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,23 @@
+str = "i want to marry sushmita"
+
+ch = str[1:4]
+
+print(ch)
+
+ch1 = str[:9]
+
+print(ch1)
+
+ch2 = str[1:]
+
+print(ch2)
+
+ch3 = str[1:9]
+
+print(ch3)
+
+len1 = len(str)
+
+print(len1)
+
+print(str)
