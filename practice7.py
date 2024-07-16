e6d4b566f420a5d7162c2725d6f169580334669e
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-17 02:48:00 +0530

    Add files via upload

---
+++ practice7.py
@@ -0,0 +1,16 @@
+a = int(input("enter an integer:"))
+b = int(input("enter an integer:"))
+
+final_result = a+b
+print(final_result)
+
+final_result1 = a*b
+print(final_result1)
+
+final_result2 =  a/b
+
+print(final_result2)
+
+final_result3 = a-b
+
+print(final_result3)
