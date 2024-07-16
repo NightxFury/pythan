26b7b5342cc92c8169ffad4e72ccc2d82eef4c54
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-16 23:56:37 +0530

    Add files via upload

---
+++ input4.py
@@ -0,0 +1,10 @@
+name = input("enter your name :")
+
+print("thank you for your name")
+
+marks = int(input("enter your marks :"))
+if marks >= 50 :
+    print("Your are pass")
+else:
+    print("Your are fail")
+    print("better luck next time")
