a5d916e01b50bc04219b68361d02d4bfd364029a
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-17 02:47:00 +0530

    Add files via upload

---
+++ practice8.py
@@ -0,0 +1,7 @@
+username = input("Enter your username :")
+
+password = input("Enter your password :")
+if username == "admin" and password == "password" :
+    print("access granted")
+    print("Your are now logged in")
+
