a40472daddef686f3c2e9d50a3fd6bb2cdd18de5
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-16 23:56:12 +0530

    Add files via upload

---
+++ input3.py
@@ -0,0 +1,13 @@
+"""
+eligible for vote
+"""
+name = (input("Enter Your Name :"))
+
+print("thank you", name)
+
+age = int(input("Enter your age:"))
+
+if age >= 18 :
+    print("your wellcome", )
+else:
+    print("You are no eligible for voting")
