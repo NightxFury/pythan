ce10688f5ca50769ff4b5d1d45b7483c9fe22317
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-16 23:55:45 +0530

    Add files via upload

---
+++ input2.py
@@ -0,0 +1,10 @@
+name  = input("Enter your name :")
+
+print("wellcome in our page", name)
+
+age = int(input("Enter your age :"))
+
+if age >= 18 :
+    print("You are old enough to vist our page")
+else:
+    print("server not lode")
