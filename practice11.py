5da4c82b750df03314a5c1f9fd4ae26e50f44087
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-19 23:57:37 +0530

    Add files via upload

---
+++ practice11.py
@@ -0,0 +1,7 @@
+number1 = input("Enter a number :")
+print("Thank you")
+number2 = input("Enter another number :4")
+print("Thank you")
+
+final_number = number1 + number2
+print("final number", final_number)
