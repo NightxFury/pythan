66f9347703d93f4de01b23ddaa4190c7643d1081
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-21 01:54:28 +0530

    Add files via upload

---
+++ statement4.py
@@ -0,0 +1,8 @@
+age =int (input("Enter your age :"))
+
+if (age >= 18) :
+    if(age >= 80) :
+        print("You are not eligible for driving")
+    print("You are eligible for driving")
+else:
+    print("You are not eligible")
