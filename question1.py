f84761c392dae6a45bca89505b6706ca466b501d
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-29 23:44:30 +0530

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,14 @@
+# swap two variables
+
+variable1 = int(input("Enter your number:"))
+
+variable2 = int(input("Enter Your number:"))
+
+swap = variable1
+
+variable1 = variable2
+
+variable2 = swap
+
+print(variable1,variable2)
+
