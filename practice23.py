aa64c63dbfe697b7d853a6824993a4b826f5b8f5
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-31 23:45:15 +0530

    Add files via upload

---
+++ practice23.py
@@ -0,0 +1,20 @@
+Movie =[]
+
+movie1 = input("enter your first movie:")
+
+movie2 = input("enter your second movie:")
+
+movie3 = input("enter your third movie:")
+
+
+
+Movie.append(movie1)
+
+Movie.append(movie2)
+
+Movie.append(movie3)
+
+print(Movie)
+
+
+
