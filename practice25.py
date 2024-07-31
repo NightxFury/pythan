c4b65c78fd2798aa7b26f2c78f6bba16c98cf8d4
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-31 23:46:07 +0530

    Add files via upload

---
+++ practice25.py
@@ -0,0 +1,6 @@
+# wap to count the number of student with "A" grade in the following tuple.and store the above values in a list and short
+
+grade =("c","D","A","B","B","A")
+print(grade.count("A"))
+grade.__str__()
+print(grade)
