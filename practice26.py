5a35103927e4751123a115f6c0df78e121705caa
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-07-31 23:46:30 +0530

    Add files via upload

---
+++ practice26.py
@@ -0,0 +1,16 @@
+tup =(1,2,3,4,5,6,7,8,9)
+print(tup[4])
+tup1 =()
+
+print(type(tup1))
+
+tup2 = (1, )
+print(tup2[0])
+print(type(tup2))
+
+#not tupple
+
+tup3 =(1)
+
+print(tup3)
+print(type(tup3))
