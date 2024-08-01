c664be9328bdd812d24420f0a44b9a680554da65
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-08-01 23:38:26 +0530

    Add files via upload

---
+++ disct2.py
@@ -0,0 +1,11 @@
+info = {
+    "name":"Rishav",
+    "Subjects":["pythonn","c","Java"],
+    "Topics":("disct","set"),
+    "age":19,
+    12.54:6544
+}
+print(info["name"])
+print(info["Subjects"])
+print(info["Topics"])
+print(info["age"])
