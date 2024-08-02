c5eddf47e674b0b28bf2cc4529b0a8cd8712fc73
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-08-02 21:05:15 +0530

    Add files via upload

---
+++ disct6.py
@@ -0,0 +1,17 @@
+student ={
+    "name":"rishav",
+    "subjects":{
+        "physics":89,
+        "chemistry":98,
+        "Mathematics":90,
+        "English":95,
+        "History":95,
+        "Geography":97,
+        "Bengali": 93
+
+}
+}
+print(list(student.values()))
+print(student.keys())
+print(len(student.values()))
+print(len(student.keys()))
