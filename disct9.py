d9cda67819734a381b55dc2f4531b58fea8c6f09
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-08-02 21:06:48 +0530

    Add files via upload

---
+++ disct9.py
@@ -0,0 +1,18 @@
+student ={
+    "name":"Rishav",
+    "Subject":{
+        "physics":89,
+        "chemistry":98,
+        "Mathematics":90,
+        "English":95,
+        "History":95,
+        "Geography":97,
+        "Bengali": 93
+
+
+}
+}
+student.update({"city":"kolkata"})
+dict = {"name":"Sushmita", "age":19}
+student.update(dict)
+print(student)
