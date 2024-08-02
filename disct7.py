3055cc41e5ea9012de37f516f7859b87a13ed9e2
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-08-02 21:06:08 +0530

    Add files via upload

---
+++ disct7.py
@@ -0,0 +1,16 @@
+student ={
+    "name":"Rishav",
+    "Subject":{
+        "physics":89,
+        "chemistry":98,
+        "Mathematics":90,
+        "English":95,
+        "History":95,
+        "Geography":97,
+        "Bengali": 93
+
+
+}
+}
+print(student.items())
+#Return all keys, value as the form of tuples
