6199bc264527e40dd7161e186c6c4dae282dfed2
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-08-02 21:43:33 +0530

    Add files via upload

---
+++ disct.py
@@ -0,0 +1,12 @@
+subjects ={}
+
+x = int(input("Enter physics:"))
+subjects.update({"physics": x})
+
+x = int(input("Enter Math :"))
+subjects.update({"Math":x})
+
+x = int(input("Enter Chemistry :"))
+subjects.update({"Chemistry":x})
+
+print(subjects)
