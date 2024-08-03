07865e194f62bde500e06976c201e0cbde5f38c2
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2024-08-03 23:42:41 +0530

    Add files via upload

--- question2.py
+++ question2.py
@@ -1,5 +1,10 @@
-side1 = float(input("Enter the value:"))
-side2 = float(input("Enter the value:"))
-side3 = float(input("Enter the value:"))
+"""
+First 10 even number
+"""
 
-print(side1 + side2 + side3)
+numbers = 2
+
+while numbers <= 20 :
+    print(numbers)
+    numbers = numbers +2
+    
